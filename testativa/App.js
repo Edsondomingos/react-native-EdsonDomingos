b165215f1b54a5289e3369ca5c255e9180da0bbe
Edson Domingos da Silva <e.domingos@academico.ifrn.edu.br>
2022-08-22 17:46:21 -0300

    correções testativa

--- testativa/App.js
+++ testativa/App.js
@@ -5,27 +5,53 @@ import { db } from "./src/config/firebaseconfig"
 
 export default function App() {
 
+  const [ id, setId ] = useState('')
+  const [ nome, setNome ] = useState('')
   const [ nomes, setNomes ] = useState('')
+
+  setId(listar())
+
+  function cadastrar(){
+    //setDoc(doc(db, "contatos", "LA"), {
+    addDoc(collection(db, "nomes"), {
+        id: nomes.length,
+        nome: nome,
+      }).then(() => {
+        //alert("Cadastrado com sucesso")
+      }).catch((error) => {
+        alert(error)
+      })
+
+      setNome('')
+}
 
   async function listar(){
     const nomesCol = collection(db, 'nomes');
     const nomesSnapshot = await getDocs(nomesCol);
     const nomesList = nomesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     setNomes(nomesList);
-    alert(nomes.nome)
+    return nomesList.length
   }
-
+  console.log(nomes.length)
+  
   return (
     <View>
+      <TextInput 
+        placeholder='Digite um nome'
+        onChangeText={setNome}
+      />
+      <Button title='Cadastrar' onPress={cadastrar}/>
       <Button title='Listar' onPress={listar}/>
-
+      <Text>Nomes:</Text>
+      
       <FlatList 
         data={nomes}
-        renderItem={({item}) => {
+        renderItem={({item}) => 
           <View>
-            <Text>nome: {item}</Text>
+            <Text>Id: {item.id}</Text>
+            <Text>nome: {item.nome}</Text>
           </View>
-        }}
+        }
       />    
       
     </View>
